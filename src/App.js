95c8022e3f2ce6fe333cfa5f7bb25ea5fa9da651
Boltea Cosmin-Robert <huliganspliff@gmail.com>
2018-08-27 19:21:25 +0300

    Simplify component with recompose

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?&key=AIzaSyCkeub2_Omf-LYhgiZ2Qa3KIIqVv78jc_M&callback=initMap",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
   <GoogleMap
     defaultZoom={17}
     defaultCenter={{ lat: 46.069923, lng: 23.561510 }}
   >
-    {props.isMarkerShown && <Marker position={{ lat: 46.070675, lng: 23.558302 }} />}
-  </GoogleMap>
-))
+  {props.isMarkerShown && <Marker position={{ lat: 46.070675, lng: 23.558302 }} />}
+</GoogleMap>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MyMapComponent
-          isMarkerShown
-          googleMapURL="https://maps.googleapis.com/maps/api/js?&key=AIzaSyCkeub2_Omf-LYhgiZ2Qa3KIIqVv78jc_M&callback=initMap"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
+        <MyMapComponent isMarkerShown />
       </div>
     );
   }
