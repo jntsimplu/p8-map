bd66f14dce60b5bc82eafc6a83865e44f55f6ad7
Boltea Cosmin-Robert <huliganspliff@gmail.com>
2018-08-29 20:58:41 +0300

    Moved map to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import Map from './map.js';
+import { locations } from './locations.js';
+import scriptLoader from 'react-async-script-loader'
+import { styles } from './mapStyle.js';
 import './App.css';
-
 import axios from 'axios'
 
 class App extends Component {
-
   state = {
-    venues: []
+    venues: [],
+    placeData: []
   }
 
   componentDidMount() {
     this.getVenues()
+  }
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?&key=AIzaSyCkeub2_Omf-LYhgiZ2Qa3KIIqVv78jc_M&callback=initMap")
+    window.initMap = this.initMap
   }
 
   getVenues = () => {
@@ -19,25 +24,83 @@ class App extends Component {
     const parameters = {
       client_id: "WYIER5VJ5T2DHNU1HPHDAHOH0FJPTD1NKMMTUSITAHNWLD5U",
       client_secret: "2ESQLERQQSVUTVAKIWXM3PFL05RIFZMHH1F5ACIHPQI54QLS",
-      query: "food",
+      query: "drinks",
       near: "Alba Iulia",
       v: "20182507"
     }
     axios.get(endPoint + new URLSearchParams(parameters))
-      .then(response => {
-        this.setState({
-          venues: response.data.response.groups[0].items
-        })
-      })
-      .catch(error => {
-        console.log("ERROR!" + error)
-      })
+    .then(response => {
+      this.setState({
+        venues: response.data.response.groups[0].items
+      }, this.renderMap())
+    })
+    .catch(error => {
+      console.log("ERROR!! " + error)
+    })
+
   }
+
+  initMap = () => {
+    const map = new window.google.maps.Map(this.refs.map, {
+      zoom: 17,
+      center: {lat: 46.069923, lng: 23.561510},
+      styles: styles
+    })
+
+    const defaultIcon = makeMarkerIcon('0091ff');
+    const highlightedIcon = makeMarkerIcon('FFFF24');
+    const infowindow = new window.google.maps.InfoWindow()
+    locations.forEach( (location) =>  {
+      location.marker = new window.google.maps.Marker({
+        position: location.pos,
+        map: map,
+        title: location.title,
+        icon: defaultIcon
+      });
+      location.marker.addListener('mouseover', function() {
+        this.setIcon(highlightedIcon);
+      });
+      location.marker.addListener('mouseout', function() {
+        this.setIcon(defaultIcon);
+      });
+      location.marker.addListener('click', function() {
+        location.marker.addListener('click', function() {
+          infowindow.setContent('<div>' + this.title + '</div>' + '<img src="https://freeiconshop.com/wp-content/uploads/edd/like-flat.png" width="50px"/>');
+          infowindow.open(map, this)
+        });
+      });
+
+    })
+  }
+
   render() {
+    const { venues, map, infowindow, bounds } = this.state;
     return (
-      <Map />
+      <div>
+      <div ref="map" id="map"></div>
+      </div>
     );
   }
 }
 
-export default App;
+function loadScript(url) {
+  var index  = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+}
+
+function makeMarkerIcon(markerColor) {
+  var markerImage = new window.google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new window.google.maps.Size(21, 34),
+    new window.google.maps.Point(0, 0),
+    new window.google.maps.Point(10, 34),
+    new window.google.maps.Size(21,34));
+    return markerImage;
+  }
+
+  export default App;
--- src/locations.js
+++ src/locations.js
@@ -1,6 +1,6 @@
 export const locations = [
-  // Caffe
   {
+    venueID: "4e8867a3dab41f4aaf6c77b6",
     title: "Enjoy Life",
     pos: {
       lat: 46.070376,
@@ -8,20 +8,15 @@ export const locations = [
     }
   },
   {
-    title: "Eis Caffe Paradis",
+    venueID: "52e94070498eccb2c7602acc",
+    title: "Gold Caffe Bar",
     pos: {
-      lat: 46.070566,
-      lng: 23.558859
+      lat: 46.069517,
+      lng: 23.563807
     }
   },
   {
-    title: "Tabu Caffe",
-    pos: {
-      lat: 46.070261,
-      lng: 23.560221
-    }
-  },
-  {
+    venueID: "4e26c5132271e4f3075a8c3c",
     title: "J'adore Caffe",
     pos: {
       lat: 46.070149,
@@ -29,13 +24,7 @@ export const locations = [
     }
   },
   {
-    title: "Blue Hours",
-    pos: {
-      lat: 46.069727,
-      lng: 23.563169
-    }
-  },
-  {
+    venueID: "50faccc8e4b0c3b20b6adcdf",
     title: "Gavroche",
     pos: {
       lat: 46.069616,
@@ -43,6 +32,7 @@ export const locations = [
     }
   },
   {
+    venueID: "519b88e2498e2e525c3b3828",
     title: "Rosenthalers",
     pos: {
       lat: 46.069187,
@@ -50,6 +40,7 @@ export const locations = [
     }
   },
   {
+    venueID: "4dada8b443a18e54131aaead",
     title: "3EST",
     pos: {
       lat: 46.069605,
@@ -57,21 +48,23 @@ export const locations = [
     }
   },
   {
+    venueID: "4d7d227d6152b60c0bb49e26",
     title: "Glamour",
     pos: {
       lat: 46.069859,
       lng: 23.564517
     }
   },
-  // Restaurant
   {
-    title: "Menuet",
+    venueID: "4d94f1bce07ea35d30859c02",
+    title: "Erol",
     pos: {
-      lat: 46.069420,
-      lng: 23.561488
+      lat: 46.070166,
+      lng: 23.558851
     }
   },
   {
+    venueID: "4d8265ed4bbaa0939551c1ac",
     title: "Go In",
     pos: {
       lat: 46.069475,
@@ -79,75 +72,11 @@ export const locations = [
     }
   },
   {
-    title: "Aloro",
+    venueID: "51b38c36498e8da9382f0a6c",
+    title: "Alloro",
     pos: {
       lat: 46.069096,
       lng: 23.564298
     }
   },
-  {
-    title: "Remeny",
-    pos: {
-      lat: 46.070590,
-      lng: 23.565056
-    }
-  },
-  {
-    title: "Ca La Bunica",
-    pos: {
-      lat: 46.070426,
-      lng: 23.557993
-    }
-  },
-   // Fast Food
-  {
-    title: "Bosfor Kebab",
-    pos: {
-      lat: 46.068787,
-      lng: 23.564320
-    }
-  },
-  {
-    title: "Panini",
-    pos: {
-      lat: 46.070539,
-      lng: 23.558295
-    }
-  },
-  {
-    title: "Dabo Doner",
-    pos: {
-      lat: 46.069350,
-      lng: 23.563688
-    }
-  },
-  {
-    title: "Broaster Chicken",
-    pos: {
-      lat: 46.069648,
-      lng: 23.564192
-    }
-  },
-  // Pizza
-  {
-    title: "Al Forno",
-    pos: {
-      lat: 46.069502,
-      lng: 23.562851
-    }
-  },
-  {
-    title: "Pizza Erol",
-    pos: {
-      lat: 46.070166,
-      lng: 23.558851
-    }
-  },
-  {
-    title: "Pizza Party",
-    pos: {
-      lat: 46.069342,
-      lng: 23.560931
-    }
-  }
 ];
